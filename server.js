7e6f8e423fda2dc93b031688df5d02835b0ee18b
bcwan <bcwan8@gmail.com>
2020-07-06 22:06:19 -0700

    Create schema

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 var express = require('express')
 var { graphqlHTTP } = require('express-graphql');
 var app = express();
-const {
+var {
   GraphQLSchema,
-  GraphQLObjectType
+  GraphQLObjectType,
+  GraphQLString
 } = require('graphql');
 
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Hello World',
+    fields: () => ({
+      message: { type: GraphQLString, 
+      resolve: () => 'Hello World'
+      }     
+    })
+  })
+})
+
 // we can access graphql without Postman
+// pass schema into here
 app.use('/graphql', graphqlHTTP({
-  graphiql: true
+  schema: schema,
+  graphiql: true,
 }));
 
 app.listen(5000, () => console.log('Server Running'))
