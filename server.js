08221dbcd04e9cfb9f157848098d7431a7a8e6fd
bcwan <bcwan8@gmail.com>
2020-07-06 22:23:01 -0700

    Create BookType and standardize RootQueryType

--- server.js
+++ server.js
@@ -4,20 +4,55 @@ var app = express();
 var {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull
 } = require('graphql');
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Hello World',
-    fields: () => ({
-      message: { type: GraphQLString, 
-      resolve: () => 'Hello World'
-      }     
-    })
+// sample data instead of hooking into database
+const authors = [
+  { id: 1, name: 'J. K. Rowling' },
+  { id: 2, name: 'J. R. R. Tolkien' },
+  { id: 3, name: 'Brent Weeks' }
+]
+
+const books = [
+  { id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+  { id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+  { id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+  { id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+  { id: 5, name: 'The Two Towers', authorId: 2 },
+  { id: 6, name: 'The Return of the King', authorId: 2 },
+  { id: 7, name: 'The Way of Shadows', authorId: 3 },
+  { id: 8, name: 'Beyond the Shadows', authorId: 3 }
+]
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  description: 'This represents a book written by an author',
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
   })
 })
 
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: 'List of All Books',
+      resolve: () => books
+    }
+  })
+})
+
+const schema = new GraphQLSchema({
+  query: RootQueryType
+})
 // we can access graphql without Postman
 // pass schema into here
 app.use('/graphql', graphqlHTTP({
