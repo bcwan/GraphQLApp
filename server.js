8c0312d58a30c80b10f60a261a80cc2fc6b5ecc3
bcwan <bcwan8@gmail.com>
2020-07-06 22:52:37 -0700

    Query single author

--- server.js
+++ server.js
@@ -63,6 +63,14 @@ const RootQueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root Query',
   fields: () => ({
+    book: {
+      type: BookType,
+      description: 'A Single Book',
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (parent, args) => books.find(book => book.id === args.id)
+    },
     books: {
       type: new GraphQLList(BookType),
       description: 'List of All Books',
@@ -72,6 +80,14 @@ const RootQueryType = new GraphQLObjectType({
       type: new GraphQLList(AuthorType),
       description: 'List of All Authors',
       resolve: () => authors
+    },
+    author: {
+      type: AuthorType,
+      description: 'An Single Author',
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (parent, args) => authors.find(author => author.id === args.id)
     }
   })
 })
